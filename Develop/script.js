99cc998a94423300e726482cfff36a673a01674d
mbatorek7 <maegan.batorek@valpo.edu>
2023-01-10 22:18:41 -0800

    added user inputs for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,43 @@
 // Assignment code here
+function generatePassword () {
+  //alphabet, num, special characters
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var special = "!@#$%^&*()";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var num = "0123456789";
 
+  // enter desired length
+  var passLength = window.prompt("Enter the desired password length:");
+  
+  // if length is less than 8 or more than 128 try again
+  if (passLength < 8 || passLength > 128) {
+    window.alert("I'm sorry. That is not a valid length. Please try again.");
+  } else {
+    //does user want lowercase, uppercase, special characters, etc?
+    var smallLetters = window.prompt("Do you want lowercase characters in your password? Enter Y/N");
+    var bigLetters = window.prompt("Do you want uppercase characters in your password? Enter Y/N");
+    var specialChar = window.prompt("Do you want special characters in your password? Enter Y/N");
+    var numeric = window.prompt("Do you want numbers in your password? Enter Y/N");
+
+    smallLetters = smallLetters.toUpperCase();
+    bigLetters = bigLetters.toUpperCase();
+    specialChar = specialChar.toUpperCase();
+    numeric = numeric.toUpperCase();
+
+    if(smallLetters != "Y" &&
+      bigLetters != "Y" &&
+      specialChar != "Y" &&
+      numeric != "Y") {
+        //user didn't choose at least one criteria
+        alert("I'm sorry. You didn't meet the criteria for a password. Please go back and choose at least one option.");
+
+    } else if () {
+        
+
+    }
+
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
